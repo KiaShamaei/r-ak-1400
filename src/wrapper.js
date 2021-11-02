a19aec2cae10a3120faea7ccae072334b20f4a8d
Asma Tanavar <a_tanavar@yahoo.com>
2021-11-02 08:06:13 +0330

    Set min-width for wrapper

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export const Wrapper = (props) => {
-    return (
-        <div className='d-flex justify-content-center align-items-center main-container '>
-            <div className='app-container br3'>
-                {props.children}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className='d-flex justify-content-center align-items-center main-container '>
+      <div className='app-container'>{props.children}</div>
+    </div>
+  );
+};
