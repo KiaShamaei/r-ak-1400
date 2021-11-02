3b5b1e2b13cfb719ce5c521b033ea55b25bb8a5d
Asma Tanavar <a_tanavar@yahoo.com>
2021-11-02 09:56:19 +0330

    Add template: Footer

--- src/shared/components/footer/Footer.js
+++ src/shared/components/footer/Footer.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import './Footer.scss';
 
+import placeholder from '../../../assets/images/icon/watch.svg';
+
 export const Footer = (props) => {
-  return <div className=''>فووتر</div>;
+  const menu = [
+    {
+      name: 'پرتفوی',
+      url: '',
+    },
+    {
+      name: 'دیده بان',
+      url: '',
+    },
+    {
+      name: 'جستجو',
+      url: '',
+    },
+    {
+      name: 'بازار',
+      url: '',
+    },
+    {
+      name: 'سفارش ها',
+      url: '',
+    },
+  ];
+
+  return (
+    <div className='p-2 d-flex justify-content-between align-items-center footer-menu'>
+      {menu.map((el, index) => {
+        return (
+          <div className='d-flex flex-column justify-content-center  align-items-center menu-item'>
+            <img src={placeholder} />
+            <p>{el.name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
